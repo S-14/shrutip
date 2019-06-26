b29d8e062ff692e8a38df700c1401f866fda0b71
user2 <user02@dypm.org>
2019-06-26 16:48:15 +0530

    modified

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Controller=function(req, res){
   console.log("Invoking  rest api for solution list");
   var employee = [
   {id:1,name:"shruti",salary:10000000},
-  {id:2,name:"smp",salary:100}
+  {id:2,name:"sp",salary:1000000}
   ];
   res.send(employee);
 };
